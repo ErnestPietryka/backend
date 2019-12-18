85796ebdd9b2ccf5c422abea2842a9ff223736da
ErnestPietryka <ernestpietryka@gmail.com>
2019-12-18 15:52:23 +0100

    app.js

--- app.js
+++ app.js
@@ -1,51 +1,22 @@
-var express=require("express"); 
-var bodyParser=require("body-parser"); 
-  
-const mongoose = require('mongoose'); 
-mongoose.connect('mongodb://localhost:27017/gfg'); 
-var db=mongoose.connection; 
-db.on('error', console.log.bind(console, "connection error")); 
-db.once('open', function(callback){ 
-    console.log("connection succeeded"); 
-}) 
-  
-var app=express() 
-  
-  
-app.use(bodyParser.json()); 
-app.use(express.static('public')); 
-app.use(bodyParser.urlencoded({ 
-    extended: true
-})); 
-  
-app.post('/sign_up', function(req,res){ 
-    var name = req.body.name; 
-    var email =req.body.email; 
-    var pass = req.body.password; 
-    var phone =req.body.phone; 
-  
-    var data = { 
-        "name": name, 
-        "email":email, 
-        "password":pass, 
-        "phone":phone 
-    } 
-db.collection('details').insertOne(data,function(err, collection){ 
-        if (err) throw err; 
-        console.log("Record inserted Successfully"); 
-              
-    }); 
-          
-    return res.redirect('signup_success.html'); 
-}) 
-  
-  
-app.get('/',function(req,res){ 
-res.set({ 
-    'Access-control-Allow-Origin': '*'
-    }); 
-return res.redirect('/index.html'); 
-}).listen(80) 
-  
-  
-console.log("server listening at port 3000"); 
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('<html><body><h1>Hello World</h1></body></html>');
+});
+
+app.post('/submit-data', function (req, res) {
+    res.send('POST Request');
+});
+
+app.put('/update-data', function (req, res) {
+    res.send('PUT Request');
+});
+
+app.delete('/delete-data', function (req, res) {
+    res.send('DELETE Request');
+});
+
+var server = app.listen(80, function () {
+    console.log('Node server is running..');
+});
